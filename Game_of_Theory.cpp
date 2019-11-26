b8c12dcbf847219dc74683c35d15cd673119ce09
Soumendu003 <soumendughorui.wb@gmail.com>
2019-11-26 12:27:32 +0530

    commit 1

--- Game_of_Theory.cpp
+++ Game_of_Theory.cpp
@@ -1,1 +1,27 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
 
+struct value{
+    uint32_t A;
+    uint32_t B;
+};
+
+
+int main(){
+    uint32_t N, i;
+
+    cin >> N;       // Takes number of Nodes
+
+    value* val_list = (value*)calloc(N,sizeof(value));
+
+    for(i = 0; i < N; i++){
+        cin >> val_list[i].A;     // Takes the A val
+    }
+
+    for(i = 0; i < N; i++){
+        cin >> val_list[i].B;     // Takes the B val
+    }
+
+    
+}
