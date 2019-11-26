f42b80a8beb20ddf9c4d5920ea0104d19da032e1
Soumendu003 <soumendughorui.wb@gmail.com>
2019-11-26 12:46:12 +0530

    Tree-built

--- Game_of_Theory.cpp
+++ Game_of_Theory.cpp
@@ -5,7 +5,28 @@ using namespace std;
 struct value{
     uint32_t A;
     uint32_t B;
+    uint32_t child_ptr;
 };
+
+struct links{
+    uint32_t child_index;
+    uint32_t next_ptr;
+};
+
+void link_edge(uint32_t u, uint32_t v, uint32_t loc, value* val_list, links* link_list){
+    if(val_list[u].child_ptr == 0){
+        link_list[loc].child_index = v;
+        link_list[loc].next_ptr = 0; // invalid ptr
+        val_list[u].child_ptr = loc;
+    }
+    else{
+        uint32_t pre_loc = val_list[u].child_ptr = loc;
+        link_list[loc].child_index = v;
+        link_list[loc].next_ptr = pre_loc;  // re-directs to previously got childs list
+        val_list[u].child_ptr = loc;
+    }
+}
+
 
 
 int main(){
@@ -13,7 +34,8 @@ int main(){
 
     cin >> N;       // Takes number of Nodes
 
-    value* val_list = (value*)calloc(N,sizeof(value));
+    value* val_list = (value*)calloc(N, sizeof(value));
+    links* link_list = (links*)calloc(N, sizeof(links));           // First entry in the link_list is invalid
 
     for(i = 0; i < N; i++){
         cin >> val_list[i].A;     // Takes the A val
@@ -23,5 +45,12 @@ int main(){
         cin >> val_list[i].B;     // Takes the B val
     }
 
-    
+    uint32_t u, v;
+    for(i = 1; i < N ; i++){      // N-1 iteration. one for each edge.
+        cin >> u;
+        cin >> v;
+        link_edge(u-1, v-1, i, val_list, link_list);
+    }
+
+
 }
